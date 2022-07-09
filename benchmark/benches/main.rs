3545f804bc793e9140d165c2562d662bc11ff4f4
Ludwig Stecher <ludwig.stecher@gmx.de>
2022-07-10 00:04:06 +0200

    Improve benchmark, add comparison to melody

--- benchmark/benches/main.rs
+++ benchmark/benches/main.rs
@@ -7,37 +7,14 @@ use pomsky::{
     Expr,
 };
 
-const STRINGS: &str = r#"'hello' "world" 'this is great!' "I absolutely love it!" '"'"#;
-
-const PROPERTIES: &str = "
-[Adlam] [Adlm] [Alphabetic] ![InBasic_Latin]
-[Tibetan] ![Tibt] [!Uppercase_Letter] [Z] ![!Zl] ![cntrl] [ascii_digit] [.] [w]
-";
-
-const GROUPS: &str = r#"
-( ( ( ( ( ( ( () ) ) ( ( ( ( ( ( ('hello') ) ) ) ) ) ) ) ) ) ( ( ( ( ( () ) ) 'world') ) ) ) )
-"#;
-
-const CAPT_GROUPS: &str = r#"
-:test(:foo(:this_is_a_really_long_capturing_group_name() :bar() :() :baz('test' :(:(:()):())):()))
-"#;
-
-const CLASSES: &str = r#"
-['a' '0'-'9' 'a'-'z' Greek !InBasic_Latin U+09-U+0D U+10FFFF '"' "'"]
-"#;
-
-const REPETITIONS: &str = r#"
-[w]{4}{3}{7,}{1,2}* [w]+ greedy [w]* lazy{500}
-"#;
-
-const SPECIAL: &str = r#"
-% !% <% %> Grapheme !>> 'this is' | 'a test'
-"#;
-
-const MODES: &str = r#"
-enable lazy; disable lazy; enable lazy; disable lazy; enable lazy;
-(disable lazy; enable lazy; 'w'+)
-"#;
+const STRINGS: &str = include_str!("./files/strings.rulex");
+const PROPERTIES: &str = include_str!("./files/properties.rulex");
+const GROUPS: &str = include_str!("./files/groups.rulex");
+const CAPT_GROUPS: &str = include_str!("./files/capt_groups.rulex");
+const CLASSES: &str = include_str!("./files/classes.rulex");
+const REPETITIONS: &str = include_str!("./files/repetitions.rulex");
+const SPECIAL: &str = include_str!("./files/special.rulex");
+const MODES: &str = include_str!("./files/modes.rulex");
 
 static SAMPLES: &[(&str, &str)] = &[
     ("strings", STRINGS),
@@ -49,6 +26,9 @@ static SAMPLES: &[(&str, &str)] = &[
     ("special", SPECIAL),
     ("modes", MODES),
 ];
+
+const VERSION_RULEX: &str = include_str!("./files/version.rulex");
+const VERSION_MELODY: &str = include_str!("./files/version.melody");
 
 pub fn parse(c: &mut Criterion) {
     let mut group = c.benchmark_group("parse");
@@ -94,6 +74,27 @@ pub fn range(c: &mut Criterion) {
     }
 }
 
+pub fn competition(c: &mut Criterion) {
+    let mut group = c.benchmark_group("version number");
+
+    group.throughput(Throughput::Bytes(VERSION_RULEX.len() as u64));
+    group.bench_function("rulex", |b| {
+        b.iter(|| {
+            Expr::parse_and_compile(
+                black_box(VERSION_RULEX),
+                Default::default(),
+                Default::default(),
+            )
+            .unwrap()
+        })
+    });
+
+    group.throughput(Throughput::Bytes(VERSION_MELODY.len() as u64));
+    group.bench_function("melody", |b| {
+        b.iter(|| melody_compiler::compiler(VERSION_MELODY).unwrap())
+    });
+}
+
 fn ruby() -> CompileOptions {
     CompileOptions { flavor: RegexFlavor::Ruby }
 }
@@ -102,6 +103,7 @@ pub fn benches(c: &mut Criterion) {
     parse(c);
     compile(c);
     range(c);
+    competition(c);
 }
 
 fn main() {
