3c6d4d3883b2eb50db6c913e919d1c7290f57db2
Ludwig Stecher <ludwig.stecher@gmx.de>
2023-03-12 22:37:05 +0100

    chore: remove unused features

--- pomsky-lib/src/diagnose/feature.rs
+++ pomsky-lib/src/diagnose/feature.rs
@@ -2,8 +2,6 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[non_exhaustive]
 pub enum Feature {
-    /// Named capturing groups, e.g. `(?<name>group)`
-    NamedCaptureGroups,
     /// Atomic groups, e.g. `(?>group)`
     AtomicGroups,
     /// Lookahead or lookbehind, e.g. `(?=lookahead)`
@@ -22,20 +20,6 @@ pub enum Feature {
     /// Forward references. They're like backreferences, but refer to a group
     /// that syntactically appears _after_ the reference
     ForwardReference,
-    /// A numeric reference relative to the current position, e.g. `\k<-2>`.
-    ///
-    /// Note that this enum variant is currently unused, because relative
-    /// references are converted to absolute references by Pomsky.
-    // TODO: maybe remove in next major version
-    RelativeReference,
-    /// A relative reference with a relative index of 0 or higher, e.g. `\k<-0>`
-    /// or `\k<+3>`. These aren't supported in any regex engine that I know
-    /// of.
-    ///
-    /// Note that this enum variant is currently unused, because relative
-    /// references are converted to absolute references by Pomsky.
-    // TODO: maybe remove in next major version
-    NonNegativeRelativeReference,
     /// Negative `\w` shorthand, i.e. `[\W]`. This is not supported in
     /// JavaScript when polyfilling Unicode support for `\w` and `\d`.
     NegativeShorthandW,
@@ -49,7 +33,6 @@ pub enum Feature {
 impl Feature {
     pub(super) fn name(self) -> &'static str {
         match self {
-            Feature::NamedCaptureGroups => "named capturing groups",
             Feature::AtomicGroups => "atomic groups",
             Feature::Lookaround => "lookahead/behind",
             Feature::Grapheme => "grapheme cluster matcher (\\X)",
@@ -58,8 +41,6 @@ impl Feature {
             Feature::SpecificUnicodeProp => "This particular Unicode property",
             Feature::Backreference => "backreference",
             Feature::ForwardReference => "forward reference",
-            Feature::RelativeReference => "relative backreference",
-            Feature::NonNegativeRelativeReference => "non-negative relative backreference",
             Feature::NegativeShorthandW => "negative `\\w` shorthand in character class",
             Feature::MixedReferences => "references to both named and numbered groups",
             Feature::RepeatedAssertion => "single repeated assertion",
